cd482a7d251da2817c404922241cad391cd45b6b
hiro <yamaoka.hiroshi.55x@st.kyoto-u.ac.jp>
2025-09-16 15:39:40 +0900

    add bottom-nav, but icon should be changed

--- run.py
+++ run.py
@@ -1,9 +1,11 @@
 import os
-from app import create_app
+from app import create_app, db
+from flask_migrate import Migrate
 
 # 環境変数'FLASK_CONFIG'があればそれを使う。なければ'default'を使う
 config_name = os.getenv('FLASK_CONFIG', 'default')
 app = create_app(config_name)
+migrate = Migrate(app, db)
 
 if __name__ == '__main__':
     app.run() # debug=True はconfigから読み込まれるので不要
