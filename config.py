960d0dfe322f46adbe5c9cd06ee1b632d301534c
hiro <yamaoka.hiroshi.55x@st.kyoto-u.ac.jp>
2025-09-17 16:00:56 +0900

    this is not good

--- config.py
+++ config.py
@@ -11,6 +11,14 @@ class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
+    MAIL_SERVER = os.environ.get('MAIL_SERVER', 'smtp.googleemail.com')
+    MAIL_PORT = int(os.environ.get('MAIL_PORT', 587))
+    MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS', 'true').lower() in ['true', 'on', '1']
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+
+    MAIL_SENDER = f"在庫管理システム <{os.environ.get('MAIL_USERNAME')}>"
+
 class DevelopmentConfig(Config):
     """
     開発環境用の設定
